8bb5879614b177afbefb3de595adb85bc19d9a0b
sanke08 <140547197+sanke08@users.noreply.github.com>
2023-08-04 06:19:51 +0530

    Update loginSignupSontroller.js

--- Controllers/loginSignupSontroller.js
+++ Controllers/loginSignupSontroller.js
@@ -48,7 +48,7 @@ exports.signin = async (req, res) => {
         if (!user) return res.status(400).json({ message: "User Not Found" })
         const isMatchPassword = await bcrypt.compare(password, user.password)
         if (!isMatchPassword) return res.status(400).json({ message: "invalid credentials" })
-        const token = jwt.sign({ id: user.id }, process.env.SECRET_KET)
+        const token = jwt.sign({ id: user._id }, process.env.SECRET_KET)
         res.cookie("token", token, {
             expires: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
             httpOnly: true
